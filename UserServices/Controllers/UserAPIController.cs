52e21521785b5e00b6f5cca71f7ee0d367a14341
Indira <indira.pasupuleti@gmail.com>
2018-04-17 12:06:37 -0500

    UserServices dlls

---
+++ UserServices/Controllers/UserAPIController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace UserServices.Controllers
+{
+    public class UserAPIController : BaseAPIController
+    {
+        public HttpResponseMessage Get()
+        {
+            return ToJson(UserDB.TblUsers.AsEnumerable());
+        }
+
+        public HttpResponseMessage Post([FromBody]TblUser value)
+        {
+            UserDB.TblUsers.Add(value);
+            return ToJson(UserDB.SaveChanges());
+        }
+
+        public HttpResponseMessage Put(int id, [FromBody]TblUser value)
+        {
+            var entity = UserDB.TblUsers.FirstOrDefault(x => x.Id == id);
+
+            
+            entity.FirstName = value.FirstName;
+            entity.LastName = value.LastName;
+            entity.Gender = value.Gender;
+
+            //UserDB.Entry(value).State = EntityState.Modified;
+            return ToJson(UserDB.SaveChanges());
+        }
+        public HttpResponseMessage Delete(int id)
+        {
+            UserDB.TblUsers.Remove(UserDB.TblUsers.FirstOrDefault(x => x.Id == id));
+            return ToJson(UserDB.SaveChanges());
+        }
+    }
+}
